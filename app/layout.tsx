82da6175dff1dd371de340e318b781d53ebb8f17
Rahul Shirode <rahul.shirode123@gmail.com>
2026-07-02 17:34:59 +0530

    Initial portfolio setup and customization

---
+++ app/layout.tsx
@@ -0,0 +1,44 @@
+import type { Metadata } from "next";
+import { Urbanist, Playfair_Display, DM_Mono } from "next/font/google";
+import "@/public/assets/css/vendors/bootstrap.min.css";
+import "@/public/assets/css/vendors/swiper-bundle.min.css";
+import "@/public/assets/css/vendors/carouselTicker.css";
+import "@/public/assets/css/vendors/magnific-popup.css";
+import "@/public/assets/fonts/remixicon/remixicon.css";
+import "@/public/assets/css/main.css";
+
+const urbanist = Urbanist({
+    weight: ["300", "400", "500", "600", "700"],
+    subsets: ["latin"],
+    variable: "--urbanist",
+    display: "swap",
+});
+const playfair_display = Playfair_Display({
+    weight: ["400", "500", "600", "700"],
+    subsets: ["latin"],
+    variable: "--playpair",
+    display: "swap",
+});
+const dmMono = DM_Mono({
+    weight: ["300", "400", "500"],
+    subsets: ["latin"],
+    variable: "--dmMono",
+    display: "swap",
+});
+
+export const metadata: Metadata = {
+    title: "Rahul Shirode - DevOps Engineer",
+    description: "Rahul Shirode - DevOps Engineer",
+};
+
+export default function RootLayout({
+    children,
+}: Readonly<{
+    children: React.ReactNode;
+}>) {
+    return (
+        <html lang="en" data-bs-theme="dark" className="zelio">
+            <body className={`${urbanist.variable} ${playfair_display.variable} ${dmMono.variable}`}>{children}</body>
+        </html>
+    );
+}
